8a3c1de4259cf7cb2027bb40df019c9502bb9b80
Dheni Subenk <dsubenk@192.168.1.4>
2022-07-11 13:13:15 +0900

    week 9 - php dasar

---
+++ cetak.php
@@ -0,0 +1,6 @@
+<?php
+
+$nama = $_POST['nama'];
+$nilai = $_POST['nilai'];
+
+echo "Halo $nama, Nilai Anda $nilai";
---
+++ week9.php
@@ -0,0 +1,19 @@
+<?php
+// $namavariabel = nilai;
+// $variabel;
+
+$nama = "Udin";
+$nama .= "Jamal";
+
+$nilai = 90.9;
+$alamat = "Ambon";
+
+$x = 10;
+$x += 50; // x = x + 50 // x = 10 + 50;
+$x += 20;
+
+//var_dump(5 > 4 xor 6 < 5);
+
+echo "Haloo $nama";
+echo "<br>";
+echo 'Haloo ' . $nama;
